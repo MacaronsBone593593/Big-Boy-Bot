ebf353a688e833332ce4bcc9632b821d802fe4f7
MacaronsBone593593 <41006055+MacaronsBone593593@users.noreply.github.com>
2018-11-20 08:47:47 +0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -7,6 +7,8 @@ module.exports.run = async (client, message, args) => {
     let breason = args.join(" ").slice(22);
     if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You do not have the permission to use this command");
     if (bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("I cannot ban them.");
+    
+    bUser.send("You have been banned from the Mickey Mouse Clubhouse https://www.youtube.com/watch?v=55-mHgUjZfY");
 
     let banEmbed = new Discord.RichEmbed()
         .setTitle("User Banned")
@@ -22,7 +24,6 @@ module.exports.run = async (client, message, args) => {
     let banchannel = message.guild.channels.find(`name`, "bot-log-channel");
     if (!banchannel) return message.channel.send("I can not find a channel to send reports");
 
-    await bUser.send("You have been banned from the Mickey Mouse Clubhouse https://www.youtube.com/watch?v=55-mHgUjZfY");
     bUser.ban(breason);
     message.delete();
     banchannel.send(banEmbed);
